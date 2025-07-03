8eb15bff162a066eb8b516f82e58bd37c6449ebe
shashankjha04 <jhashashank04@gmail.com>
2025-07-03 18:11:56 +0000

    basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail ="jhashank04@gmail.com"
+var accountPassword ="12345"
+accountCity = "Jaipur"
+
+//accountId = 2 not allowed
+accountEmail ="jhashashank04@gmail.com"
+accountPassword ="12345789"
+accountCity = "mumbai"
+console.log(accountId);
+/*
+prefer not to use var
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity ])
