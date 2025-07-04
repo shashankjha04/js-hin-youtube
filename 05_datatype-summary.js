85464e4462f2ec97052289ee7364bbe0f0be67b2
shashankjha04 <jhashashank04@gmail.com>
2025-07-04 07:40:42 +0000

    datatype summar

---
+++ 05_datatype-summary.js
@@ -0,0 +1,24 @@
+//primitive
+// 7 types : string,number,boolean,null,undefined,symbol,bigInt
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+//symbol
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id == anotherId)
+const bigNumber = 343235n
+const hero =["shaktiman","naagraj","doga"]
+let myObj={
+    name:"Shashank",
+    age: 21,
+}
+const myfunction= function(){
+    console.log("Hello world");
+}
+console.log(typeof bigNumber)
+//refrence (non primitive)
+//Array,Objects,functions
+ 
