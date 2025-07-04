358f3e1b969a0ceaf7a9826a0346554c593163b2
shashankjha04 <jhashashank04@gmail.com>
2025-07-04 06:18:42 +0000

    typeconversion

--- 01_basics/03_datatypecon.js
+++ 01_basics/03_datatypecon.js
@@ -10,3 +10,17 @@ let booleanIsLoggedIn =Boolean(isLoggedIn)
 console.log(booleanIsLoggedIn);
 // 1->true,0->false
 //null->false string->true
+// operations
+let value = 3
+let negvalue = -value
+//console.log(negvalue);
+console.log(2+2);
+console.log(2**2);
+console.log(2**3);
+//string conc
+let str1 = "Hello"
+let str2 = " Shashank"
+let str3 = str1 + str2
+console.log(str3);
+console.log("1"+2+2)
+console.log(1+2+"3")
